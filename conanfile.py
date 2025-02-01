6248fe78649252b1edf654400cc5777a9ae5502b
Aaron <56617292+SpinnerX@users.noreply.github.com>
2025-01-31 19:37:46 -0800

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+import os
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.system.package_manager import Apt, Yum, PacMan, Zypper
+from conan.tools.scm import Git
+from conan.tools.files import copy
+import os
+
+class StarterConanRecipe(ConanFile):
+    name = "example"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+    export_source = "CMakeLists.txt", "Application.cpp"
+
+    # Putting all of your build-related dependencies here
+    def build_requirements(self):
+        self.tool_requires("make/4.4.1")
+        self.tool_requires("cmake/3.27.1")
+        self.tool_requires("engine3d-cmake-utils/2.0")
+
+    # Putting all of your packages here
+    def requirements(self):
+        pass
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+    
+    def layout(self):
+        cmake_layout(self)
